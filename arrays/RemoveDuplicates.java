e2099a4e58ef0404e7b12eec8bcea0ce14212b65
KahkashanAnsari <kahkashanaaz7865@gmail.com>
2026-01-17 23:59:12 +0530

    Add program to remove duplicate elements from array

---
+++ arrays/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    ublic static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.print("Enter " +n+ " elements: ");
+        for (int i = 0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    
+}
